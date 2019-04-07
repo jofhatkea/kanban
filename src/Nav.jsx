1d967159ae833b95c3340aa503dc81ad958cb13b
Jonas Holbech <jofh@kea.dk>
2019-04-07 23:20:19 +0200

    minor changes to css for Nav component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 export default function Nav(props) {
+  const closed = true;
   return (
-    <section className="Nav closed">
+    <section className={closed ? "Nav closed" : "Nav"}>
       <button className="toggle">&#9776;</button>
       <h1>Nav</h1>
     </section>
