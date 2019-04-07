365aba5a7b2f817a791f28e072227b6283eeb086
Jonas Holbech <jofh@kea.dk>
2019-04-07 23:14:35 +0200

    menu fixed

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Nav(props) {
   return (
-    <section className="Nav">
+    <section className="Nav closed">
+      <button className="toggle">&#9776;</button>
       <h1>Nav</h1>
     </section>
   );
